705687640a52f4575791f39eb66456586fb8510c
Chris Pasti <chri.pasti@gmail.com>
2020-06-03 14:47:24 +0200

    Created view and component for app

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  sets: Set[] = [];
+
+  constructor(private apiService: ApiService) {}
+
+  getSets(): void {
+    return this.sets = this.apiService.getSets()
+    .subscribe(sets => this.sets = sets);
+  }
+
+  ngOnInit(){
+    this.getSets();
+  }
+
   title = 'frontend';
 }
